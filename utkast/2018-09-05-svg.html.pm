ae7e2d32b5297dd57228dbfa3d8f5c675145b13d
aphrodite <hugohooverwang@126.com>
2018-09-10 13:47:41 +0800

    uppdatera layout och inlägg

--- utkast/2018-09-05-svg.html.pm
+++ utkast/2018-09-05-svg.html.pm
@@ -3,11 +3,16 @@
 ◊(define-meta title "")
 ◊(define-meta author "Hugo Bernstein")
 ◊(define-meta action "")
-◊(define-meta desc "")
+◊(define-meta desc "SVG är ett XML-baserat vektorgrafik-format för tvådimensionella bilder som stöder animationer och interaktivitet.")
 
 ◊section{
+  ◊h1{SVG}
   ◊article{
     可缩放矢量图形 (SVG) 是基于矢量的图形家族的一部分。它们与基于光栅的图形不同，后者在一个数据数组中存储每个像素的颜色定义。如今，网络上使用的最常见的光栅图形格式包括 JPEG、GIF 和 PNG，每种格式都具有优缺点。
+
+    ◊p{
+      Scalable Vector Graphics (SVG) is an XML-based vector image format for two-dimensional graphics with support for interactivity and animation.
+    }
 
     ◊h3{优势}
     ◊items{
@@ -19,11 +24,19 @@
     }
     ◊h3{men vad sägs om bilder}
     Vad är bilder?
+
+    ◊h2{selections}
+    ◊em{.select()} will just select the first element that matches to the criteria specified.
   }
   ◊aside{
+    ◊(hash-ref metas 'desc)
+    ◊hr[#:class "style-one"]
     ◊h2{◊ref}
     ◊items{
+      ◊item{◊link["https://sv.wikipedia.org/wiki/Scalable_Vector_Graphics"]{Wikipedia: SVG}}
+      ◊item{◊link["https://website.education.wisc.edu/~swu28/d3t/concept.html"]{D3 Basics ---2012}}
       ◊item{◊link["https://www.ibm.com/developerworks/cn/web/wa-scalable/index.html"]{HTML5 中的可缩放矢量图形（SVG）---2012}}
+      ◊item{◊link["https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js"]{Resize svg when window is resized in d3.js}}
     }
   }
 }
